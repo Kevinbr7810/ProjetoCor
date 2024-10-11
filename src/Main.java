cefe7ec4785fe3a825c95f8979e368a4732d02f6
Kevinbr7810 <132762450+Kevinbr7810@users.noreply.github.com>
2024-10-10 22:13:25 -0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,17 @@ import FormaManager.FormaManager;
 
 public class Main {
     public static void main(String[] args) {
-        // Cria uma instância do gerenciador de formas
+      
         FormaManager formaManager = new FormaManager();
 
-        // Adiciona várias formas com diferentes cores, posições e tamanhos
+        
         formaManager.addForma("Vermelho", "10,10", 5);
         formaManager.addForma("Verde", "20,20", 10);
         formaManager.addForma("Azul", "15,15", 7);
-        formaManager.addForma("Vermelho", "30,30", 8); // Reutiliza a cor "Vermelho"
-        formaManager.addForma("Verde", "25,25", 12);   // Reutiliza a cor "Verde"
+        formaManager.addForma("Vermelho", "30,30", 8); 
+        formaManager.addForma("Verde", "25,25", 12);   
 
-        // Exibe todas as formas e suas propriedades
+      
         formaManager.apresentar();
     }
 }
